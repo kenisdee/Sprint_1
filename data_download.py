53c23b9d4e6567eb9f9113cb89c14197e81975b5
kenisdee <kdenis112@gmail.com>
2024-10-15 22:21:31 +0400

    Функционал: Вывод средней цены за период

--- data_download.py
+++ data_download.py
@@ -1,12 +1,19 @@
 import yfinance as yf
-
+import pandas as pd
 
 def fetch_stock_data(ticker, period='1mo'):
     stock = yf.Ticker(ticker)
     data = stock.history(period=period)
     return data
 
-
 def add_moving_average(data, window_size=5):
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
     return data
+
+def calculate_and_display_average_price(data):
+    if 'Close' not in data.columns:
+        print("Столбец 'Close' отсутствует в данных.")
+        return
+
+    average_price = data['Close'].mean()
+    print(f"Средняя цена закрытия акций: {average_price:.2f}")
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import data_download as dd
 import data_plotting as dplt
 
-
 def main():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
     print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
     print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
-    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
+    # Ввод тикера акции и периода
+    ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc): ")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
 
-    # Fetch stock data
+    # Загрузка данных о акциях
     stock_data = dd.fetch_stock_data(ticker, period)
 
-    # Add moving average to the data
+    # Добавление скользящего среднего к данным
     stock_data = dd.add_moving_average(stock_data)
 
-    # Plot the data
-    dplt.create_and_save_plot(stock_data, ticker, period)
+    # Вычисление и вывод средней цены закрытия акций
+    dd.calculate_and_display_average_price(stock_data)
 
+    # Построение графика данных
+    dplt.create_and_save_plot(stock_data, ticker, period)
 
 if __name__ == "__main__":
     main()
